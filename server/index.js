ce9418947df0bdffa564c9e67fba7404ec5b4c17
Emily Kwak <emilykwak1@gmail.com>
2020-03-17 21:36:17 -0700

    fetching data from discogs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 7000
+require('dotenv').config()
+
+var Discogs = require('disconnect').Client;
+
+var db = new Discogs({ userToken: process.env.TOKEN }).database();
+
+db.search('title=nirvana - nevermind')
+  .then(function (data) {
+    console.log(data)
+  })
+  .catch(err => err)
 
 app.use(express.static('public'));
 
