9c9eaf2c77b3217c9c17ffc263c3049ddf9876ed
Emily Kwak <emilykwak1@gmail.com>
2020-03-18 21:48:50 -0700

    GET request send all albums

--- server/routes/albums.js
+++ server/routes/albums.js
@@ -1,28 +1,22 @@
 const router = require('express').Router();
 require('dotenv').config()
 
-const save = require('../../database/models/Album.js')
+const save = require('../../database/models/Album.js');
+const Album = require('../../database/models/Album.js');
 
 var Discogs = require('disconnect').Client;
 
-var db = new Discogs({ userToken: process.env.TOKEN }).database();
+var discogsDB = new Discogs({ userToken: process.env.TOKEN }).database();
 
-// router.route('/search/:id').get((req, res) => {
-//   let artistTitle = req.params.id
-//   db.search(`title=${artistTitle}`)
-//     .then(function (data) {
-//       artistTitle = artistTitle.split('-')
-//       const album = data.results.filter(album =>
-//         album.title.toLowerCase().includes(artistTitle.pop())
-//       )[0]
-//       res.status(200).json(album)
-//     })
-//     .catch(err => res.status(404).json(`Error: ${err}`));
-// })
+router.route('/search').get((req, res) => {
+  Album.Album.find()
+    .then((result) => res.status(200).send(result))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+})
 
 router.route('/search/:id').post((req, res) => {
   let artistTitle = req.params.id
-  db.search(`title=${artistTitle}`)
+  discogsDB.search(`title=${artistTitle}`)
     .then(function (data) {
       artistTitle = artistTitle.split('-')
       const album = data.results.filter(album =>
