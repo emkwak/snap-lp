51b299771169aaff2072394aee37257272675c80
Emily Kwak <emilykwak1@gmail.com>
2020-03-22 17:45:19 -0700

    fetch updated data

--- client/src/components/ImageDetect.jsx
+++ client/src/components/ImageDetect.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import AlbumInfo from './AlbumInfo.jsx'
+
 const URL = "https://teachablemachine.withgoogle.com/models/UByLl9Ki/";
 
 let model, webcam, labelContainer, maxPredictions;
@@ -64,48 +66,54 @@ class ImageDetect extends React.Component {
     const prediction = await model.predict(webcam.canvas);
     for (let i = 0; i < maxPredictions; i++) {
       if (Math.round(prediction[i].probability * 10000) / 100 >= 97) {
-        this.setState({ artist: prediction[i].className, match: true }, this.check())
+        this.setState({ artist: prediction[i].className }, this.check())
       }
     }
   }
 
   check() {
     let albums = this.state.albums;
-    if (this.state.match) {
-      albums.filter(lp => {
-        let artistTitle = this.state.artist
-        if (lp.title === artistTitle) {
-          this.setState({ foundAlbum: lp })
-        }
-        else {
-          artistTitle = artistTitle.split('-').join('').replace(/ +/g, '-').toLowerCase()
-          this.setState({ id: artistTitle }, this.fetchData(this.state.id))
-        }
-      })
-    }
+    albums.filter(lp => {
+      let artistTitle = this.state.artist
+      if (lp.title === artistTitle) {
+        this.setState({ foundAlbum: lp, match: true })
+      }
+      else {
+        artistTitle = artistTitle.split('-').join('').replace(/ +/g, '-').toLowerCase()
+        this.setState({ id: artistTitle }, this.fetchData(this.state.id))
+      }
+    })
   }
 
   fetchData(id) {
-    if (this.state.match === false) {
-      fetch(`http://localhost:7000/albums/search/${id}`, {
+    {
+      this.state.foundAlbum ? fetch(`http://localhost:7000/albums/search/${id}`, {
         method: 'POST'
       })
         .then(() => {
           console.log('Success!');
+          fetch('http://localhost:7000/albums/')
+            .then(res => res.json())
+            .then((result) => this.setState({
+              albums: result
+            }))
         })
         .catch((err) => {
           console.error('Error:', err);
-        });
+        }) : null
     }
   }
 
   render() {
-    console.log(this.state)
+    const match = this.state.match
+    console.log(this.state.albums)
+    console.log(this.state.foundAlbum)
     return (
       <div>
         <div id="webcam-container"></div>
         <div id="label-container"></div>
         <button type="button" onClick={this.init} >Scan</button >
+        {match ? <AlbumInfo /> : null}
       </div>
     )
   }
